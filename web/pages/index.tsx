5a1456ec4e3d9bdff58473f63576e90299f5d03f
yufeng <321353225@qq.com>
2024-05-10 05:02:20 +0800

    page: optimize webpage and config

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -29,45 +29,62 @@ import { fromDate, getLocalTimeZone } from "@internationalized/date";
 export default function IndexPage() {
 	let [condition, setCondition] = useState("");
   return <>
-	<Input type="text" label="text" value={condition} onInput={(e) => setCondition(e.currentTarget.value)} />
-    <Table aria-label="Example static collection table">
-      <TableHeader>
-        <TableColumn>name</TableColumn>
-        <TableColumn>description</TableColumn>
-        <TableColumn>authors</TableColumn>
-		<TableColumn>keywords</TableColumn>
-		<TableColumn>repo</TableColumn>
-		<TableColumn>update_at</TableColumn>
-		<TableColumn>extra_operation</TableColumn>
-      </TableHeader>
-      <TableBody items={data.filter((item) => {
-		return item.name.match(condition) || item.description.match(condition) || item.keywords.indexOf(condition) >= 0
-	  })}>
-	  	{(item) => (
-          <TableRow key={item?.name}>
-            {/* {(columnKey) => {
-				console.log(item)
-				console.log(getKeyValue(item, columnKey));
-				return <TableCell>{getKeyValue(item, columnKey)}</TableCell>
-			}} */}
-			<TableCell>{item['name']}</TableCell>
-			<TableCell>{item['description']}</TableCell>
-			<TableCell>{item['authors'].map(({name, email}) => {
-				return <> {name};{email} <br /></>
-			})}</TableCell>
-			<TableCell>{item['keywords'].map((value) => {
-				return <><Chip style={{
-					cursor: "pointer"
-				}} onClick={() => setCondition(value)}>{value}</Chip>&nbsp;</>;
-			})}</TableCell>
-			<TableCell>{item['repo']}</TableCell>
-			<TableCell>{(new Date(item['updated_at'])).toLocaleString()}</TableCell>
-			<TableCell>
-				<Button onPress={()=>window.open(`https://github.com/${item['repo']}/issues/new/choose`)} size="sm" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">Comment</Button>
-			</TableCell>
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
+	<div style={{
+		width: '90%',
+		margin: 'auto',
+	}}>
+		<div style={{
+			padding: '1em 1em'
+		}}>
+			<Input 
+				fullWidth={true}
+				labelPlacement="outside" 
+				placeholder="请输入关键词" 
+				type="text" 
+				size="sm"
+				value={condition} 
+				onInput={(e) => setCondition(e.currentTarget.value)} 
+			/>
+		</div>
+		<Table aria-label="Example static collection table" width={"100%"}>
+			<TableHeader>
+				<TableColumn>name</TableColumn>
+				<TableColumn maxWidth={"40%"}>description</TableColumn>
+				<TableColumn>authors</TableColumn>
+				<TableColumn>keywords</TableColumn>
+				<TableColumn>repo</TableColumn>
+				<TableColumn>update_at</TableColumn>
+				<TableColumn>extra_operation</TableColumn>
+			</TableHeader>
+			<TableBody items={data.filter((item) => {
+				return item.name.match(condition) || item.description.match(condition) || item.keywords.indexOf(condition) >= 0
+			})}>
+				{(item) => (
+				<TableRow key={item?.name}>
+					{/* {(columnKey) => {
+						console.log(item)
+						console.log(getKeyValue(item, columnKey));
+						return <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+					}} */}
+					<TableCell>{item['name']}</TableCell>
+					<TableCell>{item['description']}</TableCell>
+					<TableCell>{item['authors'].map(({name, email}) => {
+						return <> {name};{email} <br /></>
+					})}</TableCell>
+					<TableCell>{item['keywords'].map((value) => {
+						return <><Chip style={{
+							cursor: "pointer"
+						}} onClick={() => setCondition(value)}>{value}</Chip>&nbsp;</>;
+					})}</TableCell>
+					<TableCell>{item['repo']}</TableCell>
+					<TableCell>{(new Date(item['updated_at'])).toLocaleString()}</TableCell>
+					<TableCell>
+						<Button onPress={()=>window.open(`https://github.com/${item['repo']}/issues/new/choose`)} size="sm" color="primary">Comment</Button>
+					</TableCell>
+				</TableRow>
+				)}
+			</TableBody>
+		</Table>
+	</div>
   </>;
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -11,6 +11,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  darkMode: "class",
+  // darkMode: "class",
   plugins: [nextui()],
 }

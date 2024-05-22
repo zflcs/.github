cf366fdc587cd6c6f4ed385d63f040c00e147e03
yufeng <321353225@qq.com>
2024-05-22 19:38:28 +0800

    ci: add mirror repos function

--- sync.py
+++ sync.py
@@ -1,28 +1,33 @@
 import subprocess
 
-# remote repo full_name, format: {{owner}}/{{repo}}
-remote_repo = "Byte-OS/test-repo-1"
-# get repository name from remote repo full_name
-repo_name = remote_repo.split("/")[-1]
+f = open("sync_list.txt", "r")
+for line in f.readlines():
+    if len(line) == 0 or line.strip() == '':
+        continue
+    print('Mirroring ' + line.strip());
+    # remote repo full_name, format: {{owner}}/{{repo}}
+    remote_repo = line.strip()
+    # get repository name from remote repo full_name
+    repo_name = remote_repo.split("/")[-1]
 
-# the result of running sync command
-sync_result = subprocess.run([
-    "gh", 
-    "repo", 
-    "sync", 
-    "kern-crates/" + repo_name
-], capture_output=True, text=True)
-
-# If the repo not exists, fork it form remote.
-if sync_result.stderr.startswith("GraphQL: Could not resolve to a Repository with the name"):
-    # fork the repository
-    subprocess.run([
+    # the result of running sync command
+    sync_result = subprocess.run([
         "gh", 
         "repo", 
-        "fork", 
-        remote_repo, 
-        "--org", 
-        "kern-crates", 
-        "--default-branch-only"
+        "sync", 
+        "kern-crates/" + repo_name
     ], capture_output=True, text=True)
-    
+
+    # If the repo not exists, fork it form remote.
+    if sync_result.stderr.startswith("GraphQL: Could not resolve to a Repository with the name"):
+        # fork the repository
+        subprocess.run([
+            "gh", 
+            "repo", 
+            "fork", 
+            remote_repo, 
+            "--org", 
+            "kern-crates", 
+            "--default-branch-only"
+        ], capture_output=True, text=True)
+        
